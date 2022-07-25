0c5f787ab2a4490b53b0430a26441629bbfa189a
laihuimei <794228073@qq.com>
2022-07-25 17:17:45 +0800

    v01

---
+++ src/main/java/com/guigu/git/App.java
@@ -0,0 +1,12 @@
+package com.guigu.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.out.println("v01");
+    }
+}
