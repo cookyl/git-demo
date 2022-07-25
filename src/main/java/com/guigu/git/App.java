e8135392ff75f1d38bd0fed88a400e92ee102440
laihuimei <794228073@qq.com>
2022-07-25 17:44:23 +0800

    v04

--- src/main/java/com/guigu/git/App.java
+++ src/main/java/com/guigu/git/App.java
@@ -6,6 +6,7 @@ package com.guigu.git;
  */
 public class App {
     public static void main( String[] args ) {
+        System.out.println("1111111111111111v04");
         System.out.println( "Hello World!" );
         System.out.println("v01");
         System.out.println("dev-v01");
