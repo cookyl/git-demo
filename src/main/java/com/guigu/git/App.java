5c72353cbf7e3ced8e1e4d406677c6743441f6d6
laihuimei <794228073@qq.com>
2022-07-25 17:24:33 +0800

    dev-v02

--- src/main/java/com/guigu/git/App.java
+++ src/main/java/com/guigu/git/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println( "Hello World!" );
         System.out.println("v01");
         System.out.println("dev-v01");
+        System.out.println("dev-v02");
     }
 }
