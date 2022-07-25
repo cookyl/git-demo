98e93a15300b85514c15c9fc03d21d4b901b2d10
laihuimei <794228073@qq.com>
2022-07-25 18:12:00 +0800

    aaaaaaaaa

--- src/main/java/com/guigu/git/App.java
+++ src/main/java/com/guigu/git/App.java
@@ -14,5 +14,6 @@ public class App {
         System.out.println("github--01");
         System.out.println("v02");
         System.out.println("dev04");
+        System.out.println("aaaaaaaaaaa");
     }
 }
